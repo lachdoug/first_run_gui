0f3b3197341fc59131bd2f716a2ba7816027a522
Lachlan Douglas <lachlan@engines.org>
2016-08-10 18:40:27 +1000

    added first_run/complete

--- start.rb
+++ start.rb
@@ -7,49 +7,66 @@ get '/' do
 end
 
 post '/submit' do
-  if submit_to_api(params)
-    redirect '/finish'
+  if post_first_run_params(params) && post_close_first_run
+    redirect '/startup'
   else
     erb :error, layout: :layout
   end
 end
 
-get '/finish' do
+get '/startup' do
   @system_status = system_status
   if @system_status == 'running'
-    @system_url = system_url
+    redirect '/finish'
   end
+  erb :startup, layout: :layout
+end
+
+get '/finish' do
+  @system_status = system_status
+  @system_url = system_url
+  log "Finished first run, redirect to system URL #{@system_url}"
   erb :finish, layout: :layout
 end
 
 def system_status
-  RestClient.get( "#{api_url}/unauthenticated/bootstrap/mgmt/status" )
+  RestClient.get( "#{api_url}/v0/unauthenticated/bootstrap/mgmt/state" )
 rescue => e
-  p "System ststus error: #{e.response}"
+  log "System status error: #{e.inspect}"
   'Error'
 end
 
 def system_url
-  RestClient.get( "#{api_url}/unauthenticated/bootstrap/mgmt/url" )
+  RestClient.get( "#{api_url}/v0/unauthenticated/bootstrap/mgmt/url" )
 rescue => e
-  p "System URL  #{e.response}"
+  log "System URL error:  #{e.inspect}"
   nil
 end
 
-def submit_to_api(params)
-
-p "url #{api_url}/system/do_first_run"
-p "api vars #{{api_vars: params}}"
-
-  result = RestClient.post( "#{api_url}/system/do_first_run", {api_vars: params}.to_json, { content_type: :json } ) == 'true'
-
-p "submit result #{result}"
-
+def post_first_run_params(params)
+  log "Post submit first run to #{api_url}/v0/system/do_first_run with api_vars: #{{api_vars: params}}"
+  result = RestClient.post( "#{api_url}/v0/system/do_first_run", {api_vars: params}.to_json, { content_type: :json } ) == 'true'
+  log "Submit first run result #{result}"
+  result
 rescue => e
-  p "e #{e.response}"
+  log "Submit first run error: #{e.inspect}"
+  false
+end
+
+def post_close_first_run
+  log "Post close first run to #{api_url}/v0/unauthenticated/bootstrap/first_run/complete"
+  result = RestClient.post( "#{api_url}/v0/unauthenticated/bootstrap/first_run/complete" ) == 'true'
+  log "Close first run result #{result}"
+  result
+rescue => e
+  log "Close first run error: #{e.inspect}"
   false
 end
 
 def api_url
-  ENV['SYSTEM_API_URL'] || "http://127.0.0.1:2380/v0"
+  ENV['SYSTEM_API_URL'] || "http://127.0.0.1:2380"
+end
+
+def log(message)
+  p message
 end
