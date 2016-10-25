35ddfeb80d52c14ab4dd26c23f2f18e191df642b
Lachlan Douglas <lachlan@engines.org>
2016-10-25 15:05:03 +1100

    never show complete error

--- start.rb
+++ start.rb
@@ -7,42 +7,69 @@ get '/' do
 end
 
 post '/submit' do
-  if post_first_run_params(params)
-    redirect "/wait?local_mgmt=#{params['local_mgmt'] == 'on'}"
+  if post_first_run_params
+    redirect "/wait?local_mgmt=#{form_strong_params[:local_mgmt]}"
   else
     erb :error, layout: :layout
   end
 end
 
 get '/wait' do
-  @local_mgmt = params['local_mgmt']
+  @local_mgmt = complete_strong_params[:local_mgmt]
   @mgmt_url = mgmt_url
   erb :wait, layout: :layout
 end
 
 get '/complete' do
-  if post_complete(params)
-    exit
+  if post_complete
+    sleep 10
   else
-    status 200
+    status 400
   end
 end
 
-def post_complete(params)
-  log "Post complete first run to #{api_url}v0/unauthenticated/bootstrap/first_run/complete"
-  result = RestClient.post( "#{api_url}v0/unauthenticated/bootstrap/first_run/complete", {api_vars: params}.to_json, { content_type: :json } )
-  log "Post complete first run result: #{result}"
+def form_strong_params
+  { admin_password: params[:admin_password].to_s,
+    admin_password_confirmation: params[:admin_password_confirmation].to_s,
+    admin_email: params[:admin_email].to_s,
+    hostname: params[:hostname].to_s,
+    local_mgmt: params[:local_mgmt].to_s == 'on',
+    networking: params[:networking].to_s,
+    domain_name: params[:domain_name].to_s,
+    self_dns_local_only: params[:self_dns_local_only].to_s == 'on',
+    dynamic_dns_provider: params[:dynamic_dns_provider].to_s,
+    dynamic_dns_username: params[:dynamic_dns_username].to_s,
+    dynamic_dns_password: params[:dynamic_dns_password].to_s,
+    ssl_person_name: params[:ssl_person_name].to_s,
+    ssl_organisation_name: params[:ssl_organisation_name].to_s,
+    ssl_city: params[:ssl_city].to_s,
+    ssl_state: params[:ssl_state].to_s,
+    ssl_country: params[:ssl_country].to_s }
+end
+
+def complete_strong_params
+  { local_mgmt: params[:local_mgmt].to_s == 'true' }
+end
+
+def post_first_run_params
+  log "Post submit first run to #{api_url}v0/system/do_first_run with api_vars: #{{api_vars: form_strong_params}}"
+  result = RestClient.post( "#{api_url}v0/system/do_first_run", {api_vars: form_strong_params}.to_json, { content_type: :json } )
+  log "Submit first run result: #{result}"
   result == 'true'
 rescue => e
   log "Submit first run error: #{e.inspect}"
   false
 end
 
-def post_first_run_params(params)
-  log "Post submit first run to #{api_url}v0/system/do_first_run with api_vars: #{{api_vars: params}}"
-  result = RestClient.post( "#{api_url}v0/system/do_first_run", {api_vars: params}.to_json, { content_type: :json } ) == 'true'
-  log "Submit first run result: #{result}"
-  result == 'true'
+def post_complete
+  log "Post complete first run to #{api_url}v0/unauthenticated/bootstrap/first_run/complete"
+  result = RestClient.post( "#{api_url}v0/unauthenticated/bootstrap/first_run/complete", {api_vars: complete_strong_params}.to_json, { content_type: :json } )
+  log "Post complete first run result: #{result}"
+  log "Post complete first run result class: #{result.class}"
+  log "Post complete first run result body: #{result.body}"
+  log "Post complete first run result body.length: #{result.body.length}"
+  # System returns empty RestClient::Response.body on success.
+  result.body == ''
 rescue => e
   log "Submit first run error: #{e.inspect}"
   false
